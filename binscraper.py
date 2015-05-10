548cdc01350144263a6b44f2cac9901b49e3c9d8
jzwang <jzwang@mit.edu>
2015-05-10 00:12:54 -0400

    Added card lengths

--- binscraper.py
+++ binscraper.py
@@ -23,8 +23,63 @@ def matchStrings(text):
             if iin in bins:
                 pass
             else: 
-                bins[iin] = 6 - j
+                bins[iin] = [6 - j]
     return bins
+
+def stringify(range):
+    list = []
+    for i in range:
+        list.append(str(i))
+    return list
+
+def addLengths(data):
+    for p in data:
+        if p[0:1] == '34' or p[0:1] == '37':
+            data[p].append(15)
+        elif p[0:2] in stringify(range(300,306)):
+            data[p].append(14)
+        elif p[0:1] == '36':
+            data[p].append(14)
+        elif p[0:1] == '54':
+            data[p].append(16)
+        elif p[0:4] == '6011':
+            data[p].append(16)
+        elif p[0:6] in stringify(range(622126,622926)):
+            data[p].append(16)
+        elif p[0:2] in stringify(range(644, 649)):
+            data[p].append(16)
+        elif p[0:1] == '65':
+            data[p].append(16)
+        elif p[0:2] in stringify(range(637, 639)):
+            data[p].append(16)
+        elif p[0:3] in stringify(range(3528, 3590)):
+            data[p].append(16)
+        elif p[0:3] == '6304' or p[0:3] == '6706' or p[0:3] == '6771' or p[0:3] == '6709':
+            data[p].append('16-19')
+        elif p[0:3] in stringify([5018, 5020, 5038, 5893, 6304, 6759, 6761, 6762, 6763]):
+            data[p].append('16-19')
+        elif p[0:1] in stringify(range(51,56)):
+            data[p].append('16-19')
+        elif p[0] == '4':
+            data[p].append('13-16')
+        elif p[0:3] in stringify([4026, 4508, 4844, 4913, 4917]):
+            data[p].append(16)
+        elif p[0:5] == '417500':
+            data[p].append(16)
+        else:
+            data[p].append(False)
+    return data
+
+def analyze(data):
+    true = 0
+    false = 0
+    for i in data:
+        if data[i][1]:
+            true += 1
+        else:
+            false += 1
+    return 'True: ' + str(true) + '\n' + 'False: ' + str(false)
+
 
 # Writes data in contents to file specified by filepath.
 # Input: filepath path to file to be written to
@@ -34,9 +89,16 @@ def writeFile(filepath, contents):
     open(filepath, 'w').close()
     # Write to file
     f = open(filepath, 'w')
-    for j in contents:
-        f.write(j + ', ' + str(contents[j]) + '\n')
+    f.write(str(contents))
+    # for j in contents:
+    #     f.write(j + ', ' + str(contents[j]) + '\n')
 
 if __name__ == '__main__':
     src = getSource("http://www.stevemorse.org/ssn/List_of_Bank_Identification_Numbers.html")
-    writeFile('bin.txt', matchStrings(src))
+    # writeFile('bin.txt', addLengths(matchStrings(src)))
+    print analyze(addLengths(matchStrings(src)))
+
+
+
+
+
