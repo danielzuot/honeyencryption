3aae1289aa6cbf20c48d648011a70368ca1830c3
jzwang <jzwang@mit.edu>
2015-05-11 02:45:11 -0400

    Fixed bin

--- binscraper.py
+++ binscraper.py
@@ -46,7 +46,7 @@ def addLengths(data, prefix_length_dict):
         for prefix in prefixes:
             for s in prefix:
                 for p in data:
-                    if p[0:len(str(s))] == str(s):
+                    if p[0:len(str(s))] == str(s) and len(data[p]) < 2:
                         data[p].append(length)
     [data[p].append(16) for p in data if len(data[p]) < 2]
     return data
@@ -65,6 +65,28 @@ def getData():
     f = open('bin.txt', 'r')
     return eval(f.read())
 
+def remove(bin):
+    bad = ['1*****', '0*****', '2*****', '3*****', '31****', '35****', '36****', '37****', '38****', '4*****', '40****', '41****', '42****', '43****', '44****', '45****', '46****', '47****', '48****', '49****', '5*****', '50****', '51****', '52****', '53****', '54****', '55****', '56****', '57****', '58****', '6*****', '60****','61****', '62****', '6200**', '6250**', '6270**', '6281**', '63****', '65****', '66****', '67****', '69****', '7*****', '89****', '8901**', '8914**', '8920**', '8934**', '8943**', '8944**', '8952**', '9*****', '9233**', '9752**', '9826**']
+    for i in bad:
+        if i in bin:
+            del bin[i]
+    return bin
+
+def removeRepetitions(bin):
+    newbin = {}
+    newbin2 = {}
+    for i in bin:
+        if '*' in i:
+            newI = re.sub('\*', '', i)
+            for j in bin:
+                if j != i:
+                    if j.startswith(newI):
+                        newbin[j] = bin[j]
+    for m in bin:
+        if m not in newbin:
+            newbin2[m] = bin[m]
+    # writeFile('bin.txt', newbin2)
+
 # Writes data in contents to file specified by filepath.
 # Input: filepath path to file to be written to
 #        contents dictionary of keys to be written
@@ -79,8 +101,10 @@ if __name__ == '__main__':
     src = getSource("http://www.stevemorse.org/ssn/List_of_Bank_Identification_Numbers.html")
     f = open('prefix_lengths.txt', 'r')
     g = open('bin.txt', 'r')
-    writeFile('bin.txt', getPrefixProbability(addLengths(matchStrings(src), eval(f.read()))))
+    # writeFile('bin.txt', getPrefixProbability(addLengths(matchStrings(src), eval(f.read()))))
     # print analyze(eval(g.read()))
+    # print eval(g.read())
+    writeFile('bin.txt', remove(eval(g.read())))
 
 
 
